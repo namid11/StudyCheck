67a162623a306783797567283c1223d36b75d15d
namid11 <namid224y@gmail.com>
2019-12-14 00:57:12 +0900

    Create WebApp

---
+++ WepApp/pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4">課題一覧</h1>
+        <p class="lead">
+          課題の確認、追加、削除ができるページです。<br>
+          このページは教師のみが閲覧できます。</p>
+        <hr class="my-3">
+        <p>More info</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Jumbo action name</a>
+        </p>
+      </div>
+    </div>
+    <div class="main container">
+      <div class="bg-white rounded box-shadow row">
+        <div class="col-md-4 col-lg-3">
+          <div class="card m-2 p-0 box-shadow">
+            <div class="card-header bg-primary text-light">
+              <h5>Subject</h5>
+            </div>
+            <div class="card-body">
+              <p class="card-text">Homework Context</p>
+              <p class="card-text">Homework Context</p>
+            </div>
+            <div class="card-footer text-muted">
+              2019/11/30
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-3">
+          <div class="card m-2 p-0 box-shadow">
+            <div class="card-header bg-primary text-light">
+              <h5>Subject</h5>
+            </div>
+            <div class="card-body">
+              <p class="card-text">Homework Context</p>
+              <p class="card-text">Homework Context</p>
+            </div>
+            <div class="card-footer text-muted">
+              2019/11/30
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-3">
+          <div class="card m-2 p-0 box-shadow">
+            <div class="card-header bg-primary text-light">
+              <h5>Subject</h5>
+            </div>
+            <div class="card-body">
+              <p class="card-text">Homework Context</p>
+              <p class="card-text">Homework Context</p>
+            </div>
+            <div class="card-footer text-muted">
+              2019/11/30
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
